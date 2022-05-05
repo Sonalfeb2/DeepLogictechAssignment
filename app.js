0fa23a899bbbadead339f2cab08db0d5c7c68e2e
Sonalfeb2 <Sonalfeb7@gmail.com>
2022-05-05 13:25:42 +0530

    Add the Comments in App.js File

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const latestStories = require("./component/latestStories");
-const featuredVoices = require("./component/featuredVoices");
+const latestStories = require("./component/latestStories"); //Include Js file which contain the json data of the latest stories
+const featuredVoices = require("./component/featuredVoices"); //Include Js file which contain the json data of the featuredVoices
 app.set("view engine", "ejs");
 app.get("/", function (req, res) {
   res.render("time", { featuredVoices, latestStories }); //for render the page with properties
